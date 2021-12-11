9bfa9688cd6b4a2f9c045e3361ada84f03ec6280
IbethPacheco22 <95945858+IbethPacheco22@users.noreply.github.com>
2021-12-10 22:05:21 -0500

    Create netsize.sh
    Modificación de pasos.

---
+++ netsize.sh
@@ -0,0 +1,6 @@
+#Deber de  Bioinformática 
+cat n10.txt
+cat ../Saavedra2013/n10.txt | wc -l
+head -n1 ../Saavedra2013/n10.txt | tr -d " " | tr -d "\ n" | wc -c
+for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; done
+En el readme estan escritos los códigos que utilicé.
